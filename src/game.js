ea4e3cbbb1e70fa75272ff1f6d44bb34d5b50455
Andrew Spano <andrew@andrews-comp.(none)>
2014-04-16 18:38:04 -0400

    added non working shift key.  hooray

--- src/game.js
+++ src/game.js
@@ -13,11 +13,14 @@ function create() {
     //game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0,0,'sky');
     player =  game.add.sprite(32, game.world.height - 150, 'player');
+    
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.physics.arcade.enable(player);
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
+    player.body.drag.set(150);
+
 
     //no animations yet.  crappy programmer art instead
 
@@ -28,10 +31,11 @@ function create() {
 
 function update () {
     var cursors = game.input.keyboard.createCursorKeys();
-    player.body.velocity.x = 0;
+    var sprintKey = game.input.keyboard.addKey(Phaser.Keyboard.shiftKey);
+   // player.body.velocity.x = 0;
 
-    console.log(player.body.touching.down);
-    
+    console.log(player.body.onFloor());
+    console.log(sprintKey);
     if(cursors.left.isDown) {
 	player.body.velocity.x = -150;
     }
@@ -39,8 +43,20 @@ function update () {
 	player.body.velocity.x = 150;
     }
 
-    if(cursors.up.isDown && player.body.touching.down) {
-	player.body.velocity.y = -350;
+    if(sprintKey.isDown) {
+	if(cursors.right.isDown) {
+	    console.log(player.body.velocity.x);
+	    player.body.velocity.x = player.velocity.x + 100000;
+	}
+
+	else if(cursors.left.isDown) {
+
+	    player.body.velocity.x = player.velocity.x - 100000;
+	    }
+    }
+
+    if(cursors.up.isDown && player.body.onFloor()) {
+	player.body.velocity.y = -320;
     }
 
     
@@ -48,7 +64,7 @@ function update () {
     
 }
 function render() {
-    Phaser.Physics.Arcade.Body.render(game.canvas, player.body,
-				      false, "#F00");
+   // Phaser.Physics.Arcade.Body.render(game.canvas, player.body,
+//				      false, "#F00");
 
 }
